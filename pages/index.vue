f7842dafedeba7e49b99336cb29d2c37b7da1d91
hiramatsu <hiramatsu3300@gmail.com>
2019-05-21 16:50:53 +0900

    test22

--- pages/index.vue
+++ pages/index.vue
@@ -32,6 +32,9 @@
       <div >
         test21  --project=$FIREBASE_PROJECT_ID
       </div>
+      <div >
+        test22  config.yml - <pre>run: echo ${ENV} > ~/.env  </pre>
+      </div>
     </div>
     <div class="content">
       <section>
